0d2e1dd0941c59d92ce162e42ffa9417ca6810b5
asepsuryana1 <asepsuryana1@gmail.com>
2020-11-23 12:39:50 +0700

    add data value /constructor props

--- todo-client/src/components/TodoBox.js
+++ todo-client/src/components/TodoBox.js
@@ -1,7 +1,18 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 
 
 export default class TodoForm extends Component {
+    onstructor(props) {
+        super(props);
+        this.state = { data: '' };
 
-    
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+
+
+    render() {
+
+    }
 }
